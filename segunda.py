4be502ccfe21bbb7796fc23f2a10c75926a03ff5
Ivan Espinoza Aguilar <cjivan560@gmail.com>
2026-08-25 18:10:52 -0600

    Agregando primeros archivos

---
+++ segunda.py
@@ -0,0 +1,16 @@
+# n es el input 
+
+n=[1,2,3,4,5,6,7] # 0(1)
+
+print("Primer for", "\n")
+for i in n:
+    print(i)# 0(n) Operacion basica
+
+print("----------------------------------------------")
+
+print("\n")
+print("Segundo for", "\n")
+for i in n:
+    print(i) # (n) Operacion basica
+
+print("---------------------------------------------")
